dcdc0323f6721505ee41567330912ea8dc0dc7cc
Fahad Riaz <fahad@axoni.com>
2019-05-04 13:36:45 -0400

    updated keymap

--- keyboards/keebio/iris/keymaps/fahadbr/keymap.c
+++ keyboards/keebio/iris/keymaps/fahadbr/keymap.c
@@ -8,13 +8,19 @@ extern keymap_config_t keymap_config;
 #define _NUMPAD 3
 
 #define KC_GRV_LWR LT(_LOWER, KC_GRV)
-#define KC_SPC_RSE LT(_RAISE, KC_SPC)
-#define KC_ENT_LWR LT(_LOWER, KC_ENT)
+#define KC_SPC_LWR LT(_LOWER, KC_SPC)
 #define KC_ESC_RSE LT(_RAISE, KC_ESC)
+#define KC_MINS_RSE LT(_RAISE, KC_MINS)
 
-#define KC_MINS_CTL CTL_T(KC_MINS)
-#define KC_DEL_ALT ALT_T(KC_DEL)
-#define KC_BSPC_GUI GUI_T(KC_BSPC)
+#define KC_ENT_CTL CTL_T(KC_ENT)
+#define KC_EQL_GUI GUI_T(KC_EQL)
+#define KC_BSPC_ALT ALT_T(KC_BSPC)
+// #define KC_DEL_ALT ALT_T(KC_DEL)
+#define KC_GRV_ALT ALT_T(KC_GRV)
+// #define KC_BSLS_ALT ALT_T(KC_BSLS)
+#define KC_BSLS_GUI GUI_T(KC_BSLS)
+// #define KC_EQL_GUI GUI_T(KC_EQL)
+#define KC_F12_CTL CTL_T(KC_F12)
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -27,27 +33,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_kc(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-      CAPS,    1,       2,       3,       4,       5,                                  6,       7,       8,       9,       0,       EQL,
+      CAPS,    1,       2,       3,       4,       5,                                  6,       7,       8,       9,       0,       DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      TAB,     Q,       W,       E,       R,       T,                                  Y,       U,       I,       O,       P,       BSLS,
+      TAB,     Q,       W,       E,       R,       T,                                  Y,       U,       I,       O,       P,      F12_CTL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      ESC_RSE,  A,       S,       D,       F,       G,                                  H,       J,       K,       L,       SCLN,    QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-      LSFT,    Z,       X,       C,       V,       B,      GRV_LWR,          DEL_ALT,  N,       M,       COMM,    DOT,     SLSH,    RSFT,
+      LSFT,    Z,       X,       C,       V,       B,      GRV_LWR,          GRV_ALT,  N,       M,       COMM,    DOT,     SLSH,    RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    LGUI,    LALT,    MINS_CTL,                  ENT_LWR, SPC_RSE, BSPC_GUI
+                                    BSLS_GUI,BSPC_ALT,ENT_CTL,                   SPC_LWR, MINS_RSE,EQL_GUI
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+     RGB_TOG, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_TOG, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_INS,                             _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_F12,
+     RGB_MOD, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_INS,                             _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_F12,
   //├────────┼────────┼────────┼────────┼────────┤────────┼                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_MUTE, KC_PSCR,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PLUS, _______,
+     RGB_RMOD,RGB_HUI, RGB_SAI, RGB_VAI, KC_MUTE, KC_PSCR,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PLUS, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______,          _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_EQL,  _______,
+     _______, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______,          _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_EQL,  _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______,  _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
