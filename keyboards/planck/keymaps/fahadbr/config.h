facab2d929b19c4a5c8f9819b54a08dea5c13712
Fahad Riaz <fahadbr@gmail.com>
2020-05-31 13:21:07 -0400

    recommitting compile commands

--- keyboards/planck/keymaps/fahadbr/config.h
+++ keyboards/planck/keymaps/fahadbr/config.h
@@ -21,7 +21,7 @@
    - MIDI notes can be sent when in Music mode is on
 */
 
-//#define MIDI_BASIC
+#define MIDI_BASIC
 
 /* enable advanced MIDI features:
    - MIDI notes can be added to the keymap
@@ -35,5 +35,5 @@
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
 
 // Most tactile encoders have detents every 4 stages
-//#define ENCODER_RESOLUTION 4
+#define ENCODER_RESOLUTION 4
 
